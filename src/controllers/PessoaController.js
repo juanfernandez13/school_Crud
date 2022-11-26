87b2e0ce75957cccf02c44b0c91bdbf7d5298b4e
Juan Pablo <juanp1310@outlook.com>
2022-11-26 18:19:26 -0300

    feat: controller getAllPeople

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -1,5 +1,15 @@
 import database from '../models';
 
 class PessoaController {
-    
+    static async getAllPeople(req,res){
+        try{
+            const allPeople = await database.Pessoas.findAll();
+            return res.status(200).json(allPeople); 
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
+
+    }
 }
+
+export default PessoaController;
