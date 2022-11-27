d8454173b9d0cce8c286a968a78511d98a7bc9aa
Juan Pablo <juanp1310@outlook.com>
2022-11-27 14:54:27 -0300

    feat: buscar uma pessoa por ID

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -8,7 +8,16 @@ class PessoaController {
         } catch (error) {
             res.status(500).json(error.message);
         }
-
+    }
+    
+    static async getPerson(req,res){
+        try{
+            const {id} = req.params;
+            const Person = await database.Pessoas.findOne( {where: {id: Number(id)}});
+            return res.status(200).json(Person); 
+        } catch (error) {
+            res.status(500).json(error.message);
+        }
     }
 }
 
