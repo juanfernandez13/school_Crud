5e328917d98660e049a06914d902c51377c18dc8
Juan Pablo <juanp1310@outlook.com>
2022-12-05 08:28:12 -0300

    feat: Adicionando nivelRouter e turmaRouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 const bodyParser = require("body-parser");
 const pessoas = require('./pessoasRoute.js');
+const turmas = require('./turmasRoute.js');
+const niveis = require('./niveisRoute.js');
 
 module.exports = app => {
     app.use(bodyParser.json());
     app.use(pessoas);
+    app.use(turmas);
+    app.use(niveis);
 };
